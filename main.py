602fd7695ace0e8141b57fc83b0b88c116c3a822
Muhammad Tomasiewicz <abdullahimuhammad269@gmail.com>
2023-05-24 18:56:30 +0000

    added sms

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-import pywhatkit as kit
+# import pywhatkit as kit
 import random
+import requests
 
 messages = ["Hello", "Hi", "Heyy", "whatsup"]
 timeHrs = random.randint(8,12)
 timeMins = random.randint(0,59)
 
+phone_number = "number"
+
 message = random.choice(messages)
 print(message)
 
-kit.sendwhatmsg("+14632748077", message, timeHrs, timeMins)
+# Send message with whatsapp
+# kit.sendwhatmsg(phone_number, message, timeHrs, timeMins)
+
+# Send SMS
+resp = requests.post('https://textbelt.com/text', {
+    'phone': phone_number,
+    'message': message,
+    'key': 'textbelt',
+})
+
+print(resp.json())
